8ae7a8f50782b28c01d7f73cdfe4c1383baf879c
William yan <1337842463@qq.com>
2023-07-03 14:25:47 +0800

    dataset & hardness

--- instructgpt.py
+++ instructgpt.py
@@ -1,7 +1,9 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config, Trainer, TrainingArguments
 from datasets import load_dataset, load_metric
+import evaluate
 import numpy as np
 import torch
+import random
 
 # Load pre-trained model and tokenizer
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
@@ -16,7 +18,7 @@ data = data['train'].train_test_split(test_size=0.1)
 
 # Define accuracy metric (Replace with your own relevant metric)
 bleu_metric = load_metric('sacrebleu')
-
+# bleu_metric = evaluate.load('sacrebleu')
 # Tokenize and format the dataset
 def format_dataset(example):
     # Combine instruction and input
@@ -75,7 +77,7 @@ training_args = TrainingArguments(
     per_device_eval_batch_size=8,  # specify the evaluation batch size if you want it to be different from the training batch size
     gradient_accumulation_steps=1,  # dafault is 1
     eval_accumulation_steps=1,  # default is 1
-    deepspeed="./ds_config_zero3.json",
+    # deepspeed="./ds_config_zero3.json",
     fp16 = True,
     include_inputs_for_metrics=True,
 )
@@ -89,28 +91,51 @@ trainer = Trainer(
     compute_metrics=compute_metrics,
 )
 
-trainer.train()
+# trainer.train()
 
-# from transformers import pipeline
-#
-# # Load your trained model and the corresponding tokenizer
-# model = GPT2LMHeadModel.from_pretrained('./test-clm/checkpoint-3645')
-# tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-#
-# # Add padding token to the tokenizer
-# tokenizer.pad_token = tokenizer.eos_token
-#
-# # Create a pipeline for text generation
-# generator = pipeline('text-generation', model=model, tokenizer=tokenizer)  # device=0 means it will run on the first GPU
-#
-# # Run inference on the model
-# input_text = tokenized_eval_dataset[0]
-# print('input_text', input_text)
-# truncated_input = input_text['input'][:tokenizer.model_max_length - 1]  # -1 for special tokens
-# output = generator(truncated_input, max_length=512, num_return_sequences=1)
-#
-#
-# print(output)
+
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+# Add padding token to the tokenizer
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.pad_token_id = tokenizer.eos_token_id
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = GPT2LMHeadModel.from_pretrained("./AI_Tutor_Training/checkpoint-2400")
+model = model.to(device)  # move model to the device
+model.eval()
+
+def generate_text(prompt_text):
+    # Encode the prompt text
+    encoded_input = tokenizer.encode(prompt_text, return_tensors='pt', max_length=512, truncation=True)
+    # Move the encoded input to the device
+    encoded_input = encoded_input.to(device)
+    # Generate text
+    output = model.generate(encoded_input, max_length=1024, do_sample=True)  # Increased max_length to 1024
+    # Decode the output
+    decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
+    return decoded_output
+
+# Load your dataset
+data = load_dataset('json', data_files='train_1890.json')
+data = data['train'].train_test_split(test_size=0.1)
+
+# Randomly select 5 samples
+random_indices = random.sample(range(len(data['train'])), 5)
+selected_samples = data['train'].select(random_indices)
+
+# Generate text for each selected sample
+for example in selected_samples:
+    prompt_text = 'Instruction: ' + example['instruction'] + ' Context: ' + example['input']
+    print('Input:\n', prompt_text)
+    print('Prediction:\n', generate_text(prompt_text))
+    print('Label:\n', example['output'])
+
+
 
 
 # deepspeed --include localhost:0,1,2,3 instructgpt.py --deepspeed ds_config_zero3.json
+# deepspeed --include localhost:0 instructgpt.py --deepspeed ds_config_zero3.json
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python instructgpt.py
